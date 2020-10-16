61451bb5fb192b23eca1d917f9e609b3fe29bdc9
lucas-garcia-reis <lucasreis.lgrf@outlook.com>
2020-10-15 21:48:19 -0300

    Atualizacao DB

--- app/__init__.py
+++ app/__init__.py
@@ -2,12 +2,16 @@ from .model import configure as config_db
 from .serializer import configure as config_ma
 from flask import Flask
 from flask_migrate import Migrate
+import os
 
 
 def create_app():
     app = Flask(__name__)
+    basedir = os.path.abspath(os.path.dirname(__file__))
 
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/crud.db'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
+        os.path.join(basedir, 'crud.db')
+    #app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/crud.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     config_db(app)
