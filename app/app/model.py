93fc9897b9d00fc099970c59472958eae897734d
Leonardo Corrêa Fossi <lcfossi@gmail.com>
2020-10-20 00:01:26 -0300

    docker-compose: debugar comunicação :bug:

---
+++ app/app/model.py
@@ -0,0 +1,59 @@
+from flask_sqlalchemy import SQLAlchemy
+import datetime
+
+db = SQLAlchemy()
+
+
+def configure(app):
+    db.init_app(app)
+    app.db = db
+
+
+# Class Sound
+class Sound(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    sound = db.Column(db.Float)
+    date = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+
+    def __init__(self, sound):
+        self.sound = sound
+
+
+# Class temperature
+class Temperature(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    temperature = db.Column(db.Float)
+    date = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+
+    def __init__(self, temperature):
+        self.temperature = temperature
+
+
+# Class Humidity
+class Humidity(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    humidity = db.Column(db.Integer)
+    date = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+
+    def __init__(self, humidity):
+        self.humidity = humidity
+
+
+# Class light
+class Light(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    light = db.Column(db.Integer)
+    date = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+
+    def __init__(self, light):
+        self.light = light
+
+
+# Class vibration
+class Vibration(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    vibration = db.Column(db.Boolean)
+    date = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+
+    def __init__(self, vibration):
+        self.vibration = vibration
---
+++ app/app/serializer.py
@@ -0,0 +1,34 @@
+from flask_marshmallow import Marshmallow
+from marshmallow import fields
+from .model import Sound, Temperature, Light, Humidity
+
+ma = Marshmallow()
+
+
+def configure(app):
+    ma.init_app(app)
+
+
+class SoundSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'sound', 'date')
+
+
+class TemperatureSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'temperature', 'date')
+
+
+class HumiditySchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'humidity', 'date')
+
+
+class LightSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'light', 'date')
+
+
+class VibrationSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'vibration', 'date')
