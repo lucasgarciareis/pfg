129e8b09e9589eddbe5af2eccd9f3de6dbbefb32
Leonardo Corrêa Fossi <lcfossi@gmail.com>
2021-02-16 20:17:41 -0300

    correções Docker, novas versões

---
+++ db/script copy.sql
@@ -0,0 +1,6 @@
+mysql -e "CREATE DATABASE IF NOT EXISTS crud;" -uroot -ppassword 
+mysql -e "create user 'projetofinal'@'%' identified by 'password';" -uroot -ppassword 
+mysql -e "grant all privileges on crud.* to 'projetofinal'@'%';" -uroot -ppassword 
+mysql -e "ALTER USER 'projetofinal'@'%' IDENTIFIED WITH mysql_native_password BY 'password';" -uroot -ppassword 
+mysql -e "USE crud;" -uroot -ppassword 
+mysql -e "FLUSH privileges;" -uroot -ppassword 
--- db/script.sql
+++ db/script.sql
@@ -1,7 +1,6 @@
-mysql -e "CREATE DATABASE IF NOT EXISTS
-    crud;" -uroot -ppassword 
-mysql -e "create user 'projetofinal'@'%' identified by 'password';" -uroot -ppassword 
-mysql -e "grant all privileges on crud.* to 'projetofinal'@'%';" -uroot -ppassword 
-mysql -e "ALTER USER 'projetofinal'@'%' IDENTIFIED WITH mysql_native_password BY 'password';" -uroot -ppassword 
-mysql -e "USE crud;" -uroot -ppassword 
-mysql -e "FLUSH privileges;" -uroot -ppassword 
+CREATE DATABASE IF NOT EXISTS crud;
+create user 'projetofinal'@'%' identified by 'password'; 
+grant all privileges on crud.* to 'projetofinal'@'%';
+ALTER USER 'projetofinal'@'%' IDENTIFIED WITH mysql_native_password BY 'password';
+USE crud;
+FLUSH privileges;
