4339d19612494148211e2eedeaaa6d927abb5c45
leo <lcfossi@gmail.com>
2021-04-01 00:23:12 -0300

    still.py functional. Still needs alert notification code

--- detect/still.py
+++ detect/still.py
@@ -4,10 +4,11 @@ import time
 
 still = 0
 count = 0
+#start_time = time.time()
 
 while 1:
-    start_time = time.time()
-    rm = requests.get(url="http://192.168.0.17:54322/movement")
+    #start_time = time.time()
+    rm = requests.get(url="http://192.168.0.17:54322/movement/still")
     list_mov = rm.json()
 
     for row in list_mov:
@@ -21,6 +22,7 @@ while 1:
         still = 1
         print("sem movimentos detectados em 30 segundos")
         # codigo de alerta
+    #print("still: {0}".format(count))
 
-    print("--- %s seconds ---" % (time.time() - start_time))
-    time.sleep(2)
+    #print("--- %s seconds ---" % (time.time() - start_time))
+    time.sleep(10)
