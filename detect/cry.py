5f2fce602273a1d53f0a99888cb591261b924976
Leonardo Corrêa Fossi <lcfossi@gmail.com>
2021-03-31 22:34:59 -0300

    cry.py now takes movement into account

--- detect/cry.py
+++ detect/cry.py
@@ -1,25 +1,34 @@
 import requests
 import json
+from time import sleep
 
-threshold = 0.05	#minimum value for crying detection
-count = 0	#amount of instances where threshold was surpassed
-max = 3		#maximum amount of  instances of threshold surpassed
-noise = 0		#flag for noise
-movement = 0		#flag for movement
+threshold = 460  # minimum value for crying detection
+count = 0  # amount of instances where threshold was surpassed
+max = 3  # maximum amount of  instances of threshold surpassed
+noise = 0  # flag for noise
+cry = 0  # flag for crying when there's noise AND movement
 
-# while 1:
-r = requests.get(url="http://192.168.0.17:54322/sound")
-list = r.json()
+while 1:
+    r = requests.get(url="http://192.168.0.17:54322/sound")
+    list = r.json()
 
-for row in list:
-	if(row['sound'] > threshold):
-		count+=1	
-	
-	if(count >= max): 
-		noise = 1
+    for row in list:
+        if(row['sound'] > threshold):
+            count += 1
 
-if(noise==1):
-	print("telegram avisa que chorou\n")
-	print("count = {0}".format(count))
-	noise = 0 
-	count = 0	
+        if(count >= max):
+            noise = 1
+
+    if(noise == 1):
+        print("barulho detectado\n")
+        print("pontos acima do threshold = {0}".format(count))
+        noise = 0
+        count = 0
+        r = requests.get(url="http://192.168.0.17:54322/movement")
+        list = r.json()
+        for row in list:
+            if(row['movement'] == 1):
+                cry = 1
+                break
+
+    sleep(2)
