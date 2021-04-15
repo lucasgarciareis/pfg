25bc636cfaf54ef5154291224fea43da9ef37e52
Leonardo Corrêa Fossi <lcfossi@gmail.com>
2021-04-14 23:36:40 -0300

    Revert "fixed cry"
    This reverts commit f0e4dde624f0e2ec2641bc82d6cdd90c36bd362b.

--- detect/cry/cry.py
+++ detect/cry/cry.py
@@ -142,6 +142,5 @@ while 1:
                 count = 0
                 time.sleep(20)
                 break
-    count = 0
     #print("--- %s seconds ---" % (time.time() - start_time))
     time.sleep(2)
